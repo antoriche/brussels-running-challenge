4c30917ec5e5a1a9285c682dec226040df33d470
Antonin Riche <antonin@jetpack.ai>
2025-01-20 13:31:50 +0100

    using strava api from frontend

--- app/src/hooks/activities.tsx
+++ app/src/hooks/activities.tsx
@@ -1,8 +1,45 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAPI } from "../services/api";
 import { queryClient } from "../App";
-import { StravaListedActivity } from "../services/strava";
+import { StravaApi, StravaListedActivity } from "../services/strava";
+import { getOAuth } from "../services/auth";
 
+export const getActivitiesFromStrava = async () => {
+  const oauth = await getOAuth();
+  if (!oauth) throw new Error("Not logged in");
+  const strava = new StravaApi(`Bearer ${oauth.access_token}`);
+  let query: Awaited<ReturnType<StravaApi["listActivities"]>> = [];
+
+  let queryTmp: typeof query = [];
+  let i = 1;
+  const PER_PAGE = 200;
+  do {
+    queryTmp = await strava.listActivities({ page: i, per_page: PER_PAGE });
+    query = query.concat(queryTmp);
+    i++;
+  } while (queryTmp.length > 0 && queryTmp.length === PER_PAGE);
+  //query = await strava.listActivities({ page: 1, per_page: 5 });
+
+  query = query.filter((activity) => activity.type === "Run" && activity.location_country === "Belgium");
+
+  query = await Promise.all(
+    query.map(async (activity) => {
+      //const details = await strava.getActivity(activity.id);
+      const gpx = null; // await strava.getGPX(activity.id);
+      return {
+        ...activity,
+        gpx,
+        //map: details.map,
+      };
+    }),
+  );
+  return query;
+};
+
+/**
+ * get activities from api
+ * @deprecated
+ */
 export const getActivities_ = async () => {
   const api = await getAPI();
   const res = await api.get<
@@ -19,6 +56,6 @@ export const getActivities_ = async () => {
   return res.data;
 };
 
-export const useActivities = () => useQuery({ queryKey: ["strava", "activities"], queryFn: () => getActivities_() });
+export const useActivities = () => useQuery({ queryKey: ["strava", "activities"], queryFn: () => getActivitiesFromStrava() });
 
-export const getActivities = () => queryClient.fetchQuery({ queryKey: ["strava", "activities"], queryFn: () => getActivities_() });
+export const getActivities = () => queryClient.fetchQuery({ queryKey: ["strava", "activities"], queryFn: () => getActivitiesFromStrava() });
